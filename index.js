8bdf481068e47bb863cefec64ba2465ed8ea1926
IfeoluwaAroboto <ifeee@gmail.com>
2022-05-11 23:04:13 +0100

    added j

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 let thanksEl = document.getElementById("thanks-el")
+const b = document.querySelector (".b")
 thanksEl.addEventListener("click", function(){
-    alert("thanks for participating")
+    if (b.classList.contains("remove-b"))
+    { b.classList.remove ("remove-b")
+
+    }
+    else {b.classList.add ("remove-b")}
 }) 
     
